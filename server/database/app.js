b70ef2813e1c878c7c8b89ef5d5549bd09e17ede
nicholasneo78 <nicholasneo78@gmail.com>
2024-03-22 12:23:04 -0400

    Week 3a update

--- server/database/app.js
+++ server/database/app.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require('mongoose').Types;
 const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
@@ -59,16 +60,42 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
 //Write your code here
+  try {
+    const documents = await Dealerships.find();
+    res.json(documents);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
 //Write your code here
+  try {
+    const documents = await Dealerships.find({ state: req.params.state });
+    res.json(documents);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
 //Write your code here
+  try {
+
+    const document = await Dealerships.find({id: req.params.id});
+    if (!document) {
+      return res.status(404).json({ error: 'Document not found' });
+    }
+    
+    res.json(document);
+  } catch (error) {
+    console.error('Error fetching document:', error);
+    res.status(500).json({ error: 'Error fetching document' });
+  }
 });
 
 //Express route to insert review
